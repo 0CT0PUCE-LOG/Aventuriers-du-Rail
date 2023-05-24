1dd1e8783b14ab4339cb4b9b2b0d776db0768e5d
starcks <soren.starck@etu.umontpellier.fr>
2023-05-24 11:45:41 +0200

    fxml working

--- src/main/java/fr/umontpellier/iut/rails/vues/VueDuJeu.java
+++ src/main/java/fr/umontpellier/iut/rails/vues/VueDuJeu.java
@@ -6,10 +6,13 @@ import fr.umontpellier.iut.rails.mecanique.data.Destination;
 import javafx.collections.ListChangeListener;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+
+import java.io.IOException;
 
 /**
  * Cette classe correspond à la fenêtre principale de l'application.
@@ -29,6 +32,14 @@ public class VueDuJeu extends VBox {
 
     public VueDuJeu(IJeu jeu) {
         this.jeu = jeu;
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/VueDuJeu.fxml"));
+            loader.setRoot(this);
+            loader.setController(this);
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         /*
         plateau = new VuePlateau();
         getChildren().add(plateau);

14d1d988b4ba00c784c6b3aebde517f227d1621d
Norman Francois <norman.francois@etu.umontpellier.fr>
2023-05-25 16:09:00 +0200

    Implémentation VueCarteTransport

--- src/main/java/fr/umontpellier/iut/rails/vues/VueCarteTransport.java
+++ src/main/java/fr/umontpellier/iut/rails/vues/VueCarteTransport.java
@@ -1,6 +1,8 @@
 package fr.umontpellier.iut.rails.vues;
 
 import fr.umontpellier.iut.rails.ICarteTransport;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
 /**
@@ -8,12 +10,42 @@ import javafx.scene.layout.Pane;
  *
  * On y définit le listener à exécuter lorsque cette carte a été choisie par l'utilisateur
  */
-public class VueCarteTransport extends Pane {
+public class VueCarteTransport extends Label {
 
     private final ICarteTransport carteTransport;
 
     public VueCarteTransport(ICarteTransport carteTransport, int nbCartes) {
         this.carteTransport = carteTransport;
+        setGraphic(new ImageView(getFichierCarteTransport()));
+    }
+
+    private String getFichierCarteTransport(){
+        String file = "cartesWagons/carte-";
+
+        if(carteTransport.estBateau()){
+            if(carteTransport.estDouble()){
+                file+="DOUBLE-";
+            }
+            else{
+                file+="BATEAU-";
+            }
+        }
+        else if(carteTransport.estWagon()){
+            file+="WAGON-";
+        }
+        else{
+            file+="JOKER-";
+        }
+
+        file+= carteTransport.getStringCouleur();
+
+        if(carteTransport.getAncre()){
+            file+="A";
+        }
+
+        file+=".png";
+
+        return file;
     }
 
     public ICarteTransport getCarteTransport() {
